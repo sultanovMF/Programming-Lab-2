e16f9029b5850153e926abc72bd8ad960b3f94e0
Мурат Султанов <sultanov.murat-f@net.ugatu.su>
2021-11-12 20:43:53 +0500

    fixed bugs

--- GraphObject.cs
+++ GraphObject.cs
@@ -1,27 +1,31 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 
 namespace WinFormsApp1 {
     class GraphObject {
-        static System.Random r = new System.Random();
-        int x {
+        static Random r = new Random();
+        private int x;
+
+        int X {
             get { return x; }
             set {
-                if (value < 0) { throw new System.ArgumentException("x<0!"); }
                 x = value;
             }
         }
-        int y {
+
+        int Y {
             get { return y; }
             set {
-                if (value < 0) { throw new System.ArgumentException("x<0!"); }
                 y = value;
             }
-        }
+        } 
+        public int y;
         int h;
         int w;
         SolidBrush brush;
         public GraphObject() {
-            Color[] cols = { Color.Red, Color.Green, Color.Yellow, Color.Tomato,Color.Cyan };
+           
+            Color[] cols = { Color.Red, Color.Green, Color.Yellow, Color.Tomato, Color.Cyan };
             var c = cols[r.Next(cols.Length)];
             x = r.Next(200);
             y = r.Next(200);
